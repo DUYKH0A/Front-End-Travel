f1b28311d017ffcbd6fab5ba4912674f1472b295
Nguyễn Duy Khoa <duykhoa24082002@gmail.com>
2024-07-08 14:59:47 +0700

    delete config

--- src/utils/config.js
+++
@@ -1,5 +0,0 @@
-
-export const BASE_URL = 'http://localhost:4000/api/v1'
-export const REACT_APP_LIMIT =5
-export const REACT_APP_CLOUD_NAME='dszv4eup6'
-export const REACT_APP_UPLOAD_ASSETS_NAME='demo-upload'
